cadb64e0555e13bee0ba4df4418ac4bdbececcfc
Zeerex23 <tobilobadiekola@gmail.com>
2022-03-11 10:22:47 +0100

    Refactor: Fixing Responsiveness

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,28 @@
-// NavBar Shadow
+// Loading up the Page
+window.onload = () => {
+  document.querySelector('header').style.opacity = '1'
+  document.querySelector('main').style.opacity = '1'
+  document.querySelector('footer').style.opacity = '1'
+}
+
+// NavBar Shadow & Stickiness
 window.onscroll = () => {
   let navBar = document.querySelector('#navBar');
   let lastScrollTop = 0;
   let dir = window.scrollY || document.documentElement.scrollTop;
   
   if (dir > lastScrollTop){
-    navBar.classList.add('shadow-lg', 'headerOpc')
+    navBar.classList.add('shadow-lg')
+    navBar.style.backgroundColor = 'rgba(10, 25, 47, .85)'
+    navBar.style.backdropFilter = 'blur(12px)'
+    navBar.style.webkitBackdropFilter = 'blur(12px)'
+    navBar.style.height = '8vh'
   } else {
-    navBar.classList.remove('shadow-lg', 'headerOpc')
+    navBar.classList.remove('shadow-lg')
+    navBar.style.backgroundColor = 'transparent'
+    navBar.style.backdropFilter = 'none'
+    navBar.style.webkitBackdropFilter = 'none'
+    navBar.style.height = 'initial'
   }
 }
 
@@ -31,4 +46,17 @@ navLinks.forEach( link => {
       link.classList.remove('active')
     }
   })
+})
+
+// NavBar Toggling Button
+let navBtn = document.querySelector('#navbar-toggler');
+navBtn.addEventListener('click', () => {
+  if ( navBtn.classList.contains('bi-list') ) {
+    navBtn.classList.remove('bi-list')
+    navBtn.classList.add('bi-x-lg')
+  } else {
+    navBtn.classList.add('bi-list')
+    navBtn.classList.remove('bi-x-lg')
+  }
+  
 })
